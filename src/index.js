480ca721190654c7829635d58896462789dd1f5b
Kev Comedia <kevcomedia@gmail.com>
2017-09-06 19:36:28 +0800

    Display bar chart from data
    I don't understand why `const d3 = require('d3');` works, but
    `import d3 from 'd3';` doesn't. It looks like more fiddling with the
    webpack config is needed to make the latter work.
    
    Since the data is in out repo, there's no need to use `d3.json`.
    
    It turns out I had to add another setting in the `parserOptions` in the
    .eslintrc file, else the imports won't work.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-const message = 'Hello World';
+const d3 = require('d3');
+import {data} from '../data/GDP-data.json';
 
-const p = document.createElement('p');
-p.innerHTML = message;
+const width = 1000;
+const height = 500;
+const padding = 50;
 
-document.body.appendChild(p);
+const barWidth = width / data.length;
+
+const svg = d3.select('#chart')
+  .attr('width', width)
+  .attr('height', height);
+
+const xScale = d3.scaleTime()
+  .domain([new Date(data[0][0]), new Date(data[data.length - 1][0])])
+  .range([padding, width - padding]);
+
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(data, (d) => d[1])])
+  .range([height - padding, padding]);
+
+svg.selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('fill', 'black')
+  .attr('x', (d) => xScale(new Date(d[0])))
+  .attr('y', (d) => yScale(d[1]))
+  .attr('width', barWidth)
+  .attr('height', (d) => height - padding - yScale(d[1]));
