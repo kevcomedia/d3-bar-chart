e5be4beb5ceef2ff12d7ef928e07f86b205cd377
Kev Comedia <kevcomedia@gmail.com>
2017-09-10 09:37:06 +0800

    Split padding into four values

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ import {createTooltip} from './tooltip.js';
 
 const width = 1000;
 const height = 500;
-const padding = 50;
+const padding = {
+  bottom: 50,
+  left: 50,
+  right: 15,
+  top: 25,
+};
 
 const barWidth = width / data.length;
 
@@ -18,20 +23,20 @@ const svg = d3.select('#chart')
 
 const xScale = d3.scaleTime()
   .domain([new Date(data[0][0]), new Date(data[data.length - 1][0])])
-  .range([padding, width - (padding / 2)]);
+  .range([padding.left, width - padding.right]);
 
 const yScale = d3.scaleLinear()
   .domain([0, d3.max(data, (d) => d[1])])
-  .range([height - padding, padding / 2]);
+  .range([height - padding.bottom, padding.top]);
 
 // Bottom time axis
 svg.append('g')
-  .attr('transform', `translate(0, ${height - padding})`)
+  .attr('transform', `translate(0, ${height - padding.bottom})`)
   .call(d3.axisBottom(xScale));
 
 // Left GDP axis
 svg.append('g')
-  .attr('transform', `translate(${padding}, 0)`)
+  .attr('transform', `translate(${padding.left}, 0)`)
   .call(d3.axisLeft(yScale));
 
 const tooltip = createTooltip();
@@ -43,7 +48,7 @@ svg.selectAll('rect')
   .attr('x', (d) => xScale(new Date(d[0])))
   .attr('y', (d) => yScale(d[1]))
   .attr('width', barWidth)
-  .attr('height', (d) => height - padding - yScale(d[1]))
+  .attr('height', (d) => height - padding.bottom - yScale(d[1]))
   .on('mouseover', (d) => {
     tooltip.setData(d)
       .setLocationOnPage(d3.event)
